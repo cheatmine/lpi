135452862ef6c610b14aea5806ef910c0657bc86
CheatMine <fastest.zen@gmail.com>
2025-01-25 18:30:38 +0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,8 +4,8 @@ local Player = Players.LocalPlayer
 
 -- Init lib
 local rep = "https://github.com/cheatmine/lpi/raw/refs/heads/main"
-local LPI = loadstring(game:HttpGet(rep.."/API.lua"))
-local UI = loadstring(game:HttpGet(rep.."/UILib.lua"))()
+local LPI = loadstring(game:HttpGet(rep.."/API.lua", false), "LPI")()
+local UI = loadstring(game:HttpGet(rep.."/UILib.lua", false), "UI")()
 local Notif = UI:InitNotifications()
 
 local blacklisted = { -- blacklisted tools
@@ -50,7 +50,7 @@ end
 
 -- Services
 local events = {}
-services.ToolBlacklist = function()
+services.ToolBlacklist = {
 	start = function()
 		events.NoTools = {}
 		local function BindTool(tool, character)
@@ -98,7 +98,7 @@ services.ToolBlacklist = function()
 			v:Disconnect()
 		end
 	end
-end
+}
 
 -- UI
 UI.title = "LPI Client"
@@ -106,42 +106,54 @@ UI:Introduction()
 
 local Window = UI:Init(Enum.KeyCode.RightControl)
 
-local Wm = library:Watermark("LPI Client | v1.0 | " .. library:GetUsername())
-local FpsWm = Wm:AddWatermark("fps: " .. library.fps)
+local Wm = UI:Watermark("LPI Client | v1.0 | " .. UI:GetUsername())
+local FpsWm = Wm:AddWatermark("fps: " .. UI.fps)
 
 coroutine.wrap(function()
 	while wait(.75) do
-		FpsWm:Text("fps: " .. library.fps)
+		FpsWm:Text("fps: " .. UI.fps)
 	end
 end)()
 
 -- Tabs
-local world = Window:NewTab("World")
+local tab = Window:NewTab("World")
 
-world:NewSection("Tools")
-world:NewButton("Init BTools API", function()
-	LPI.Workspace.GrabBTools()
-	LPI.Workspace.Init()
-end)
-world:NewToggle("Tool Blacklist", false, function(bool)
+tab:NewToggle("Tool Blacklist", false, function(bool)
 	if bool then
 		startService("ToolBlacklist")
 	else
 		stopService("ToolBlacklist")
 	end
 end)
-world:NewButton("Get F3X", function()
+tab:NewButton("Init BTools API", function()
+	if not workspace:FindFirstChild("D") then
+		LPI.Workspace.GrabBTools()
+	end
+	LPI.BTools.Init()
+end)
+tab:NewButton("Get F3X", function()
 	LPI.Workspace.GrabF3X()
 end)
-world:NewButton("Get BTools", function()
+tab:NewButton("Get BTools", function()
 	LPI.Workspace.GrabBTools()
 end)
+tab:NewSection("Parts")
+tab:NewButton("Delete all spawns", function()
+	for i, v in workspace:GetChildren() do
+		if v.Name == "SpawnLocation" then
+			LPI.BTools.DestroyInstance(v)
+		end
+	end
+end)
 
-world:NewSection("Players")
-world:NewButton("Kill All", function()
+local tab = Window:NewTab("Players")
+tab:NewButton("Kill All", function()
 	for i, v in Players:GetPlayers() do
 		if not match(v.Name, exempt) and v.Character then
 			LPI.BTools.Kill(v.Character)
 		end
 	end
 end)
+
+
+Notif:Notify("Loaded LPI Client", 4, "success")
